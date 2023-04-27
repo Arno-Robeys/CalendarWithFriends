aa4d252b2ed2c662193467de8d601a7a13fd51b3
Ninoverstraeten2003 <r0898606@ucll.be>
2023-04-27 19:22:13 +0200

    commented table

--- lib/calendarwithfriends_web/live/event_live/show.ex
+++ lib/calendarwithfriends_web/live/event_live/show.ex
@@ -20,6 +20,17 @@ defmodule CalendarwithfriendsWeb.EventLive.Show do
      )}
   end
 
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    event = Events.get_event!(id)
+    {:ok, _} = Events.delete_event(event)
+    {:noreply, assign(socket, :events, list_events())}
+  end
+
+  defp list_events do
+    Events.list_events()
+  end
+
   defp page_title(:show), do: "Show Event"
   defp page_title(:edit), do: "Edit Event"
 end
