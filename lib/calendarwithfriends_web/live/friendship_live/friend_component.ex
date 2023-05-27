b21b64ff591fd814cb0bc888686e165a750a52cf
Ninoverstraeten2003 <r0898606@ucll.be>
2023-05-27 23:29:09 +0200

    better visual

--- lib/calendarwithfriends_web/live/friendship_live/friend_component.ex
+++ lib/calendarwithfriends_web/live/friendship_live/friend_component.ex
@@ -3,8 +3,17 @@ defmodule CalendarwithfriendsWeb.FriendshipLive.FriendComponent do
 
   def render(assigns) do
     ~H"""
-    <div id={"friendships-#{@friendship.id}"}>
-      You're friends with <%= @full_name %>
+    <div id={"friendships-#{@friendship.id}"} class="bg-white shadow-lg rounded-lg px-6 py-3 mb-2">
+      <div class="flex items-center justify-between">
+        <div class="flex items-center">
+          <div class="h-10 w-10 rounded-full bg-gray-300"></div>
+
+          <div class="ml-4">
+            <div class="font-semibold text-lg text-gray-800"><%= @full_name %></div>
+            <div class="text-gray-500">Online</div>
+          </div>
+        </div>
+      </div>
     </div>
     """
   end
